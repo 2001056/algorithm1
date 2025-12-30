fdaa5761da53ff928e89514ca8a27d1aacf1bf8e
2001056 <lbchan1027@gmail.com>
2025-12-30 09:49:18 +0900

    a

--- src/A2dfs/A02이차원배열의완전탐색.java
+++ src/A2dfs/A02이차원배열의완전탐색.java
@@ -1,4 +1,8 @@
 package A2dfs;
 
 public class A02이차원배열의완전탐색 {
+    public static void main(String[] args) {
+        int[][] arr = {{1,1,1,1},{1,1,1,1},{1,1,2,1},{1,1,1,1}};
+        int[] current = {0,0};
+    }
 }
